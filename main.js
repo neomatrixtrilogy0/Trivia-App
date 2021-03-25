a370abc8d67781e7e0ae00db1ba97ad19c5b9771
neo <neo@neo>
2021-03-24 23:08:03 -0400

    Started working on the JS portion and finished making the questions.

---
+++ main.js
@@ -0,0 +1,41 @@
+const gotQuiz = [
+    {
+        question: 'Which of the following is not Dany\'s Dragons? ',
+        a: 'Drogo',
+        b: 'Rhaegal',
+        c: 'Balerion',
+        d: 'Viserion',
+        correct: 'c'
+    },
+    {
+        question: 'Who was Sansa\'s first husband?',
+        a: 'Ramsey',
+        b: 'Gendry',
+        c: 'Joffrey',
+        d: 'Tyrion',
+        correct: 'd'
+    },
+    {
+        question: 'Where are the faceless men from?',
+        a: 'Braavos',
+        b: 'Winterfell',
+        c: 'Qarth',
+        d: 'King\'s landing',
+        correct: 'a'
+    },
+    {
+        question: 'What did you think of the last season?',
+        a: 'Yayyyy',
+        b: 'Crap',
+        correct: 'b'
+    },
+    {
+        question: 'Who were you rooting for to sit on the throne?',
+        a: 'Cersei',
+        b: 'Dany',
+        c: 'Jon',
+        d: 'Sansa',
+        correct: 'b'
+    }
+
+]
