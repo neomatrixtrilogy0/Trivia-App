ce88f319be075f0a448b03dcaa968b988a02d062
neo <neo@neo>
2021-03-27 19:07:26 -0400

    Finished coding the trivia quiz game about GoT

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a trivia game about Game of Thrones. Some of the answers are based on my feelings on the show. 
--- main.js
+++ main.js
@@ -27,6 +27,8 @@ const gotQuiz = [
         question: 'What did you think of the last season?',
         a: 'Yayyyy',
         b: 'Crap',
+        c: 'OK',
+        d: 'Great',
         correct: 'b'
     },
     {
@@ -39,3 +41,69 @@ const gotQuiz = [
     }
 
 ]
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentgotQuiz = gotQuiz[currentQuiz];
+
+    questionEl.innerText = currentgotQuiz.question;
+    a_text.innerText = currentgotQuiz.a;
+    b_text.innerText = currentgotQuiz.b;
+    c_text.innerText = currentgotQuiz.c;
+    d_text.innerText = currentgotQuiz.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === gotQuiz[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < gotQuiz.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly ${score}/${gotQuiz.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>`;
+            
+        }
+    }
+});
